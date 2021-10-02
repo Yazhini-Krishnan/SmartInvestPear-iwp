54761b092d50ff59f730cc72038bf030c11a912a
Yazhini-Krishnan <68785790+Yazhini-Krishnan@users.noreply.github.com>
2021-10-02 11:25:42 +0530

    verification-success

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -5,7 +5,13 @@ def home(request):
     import requests
     import json
     api_requests=requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_500423e7a0af4ae7a089eede072b52a1")
-    return render(request, 'home.html', {})
+    
+    try:
+        api=json.loads(api_requests.content)
+
+    except Exception as e:
+        api = "error"
+    return render(request, 'home.html', {'api' : api})
 
 def about(request):
     return render(request, 'about.html', {})
